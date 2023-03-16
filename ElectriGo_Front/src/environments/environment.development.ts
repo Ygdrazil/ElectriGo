1e4df60622bfb7119c0a9cdeb8d662c58c177bdc
Ygdrazil <vfz1204@gmail.com>
2023-03-16 08:26:58 +0100

    fixed blocked mixed content

--- ElectriGo_Front/src/environments/environment.development.ts
+++ ElectriGo_Front/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     borneApiUrl: "https://odre.opendatasoft.com/api/records/1.0/search/",
     geocoderApiUrl: "https://nominatim.openstreetmap.org/",
-    routingApiUrl: "http://router.project-osrm.org/route/v1/",
+    routingApiUrl: "https://router.project-osrm.org/route/v1/",
     vehicleApiUrl: "https://api.chargetrip.io/graphql",
     vehicleProjectId: "641185f47777bc4ea27cfd21",
     vehicleAppId: "641185f47777bc4ea27cfd23",
